221802f5f7c8d5e30cf2eeb6a4c1db7f43d5f702
Heang Ly <heang.ly@zoom.us>
2025-06-06 22:27:20 -0400

    0.16.30

--- dist/sdk.es.js
+++ dist/sdk.es.js
@@ -1,4 +1,4 @@
-/* Zoom Apps SDK v0.16.29  */
+/* Zoom Apps SDK v0.16.30  */
 /**
  * Copyright (c) 2025 Zoom Video Communications, Inc.
  * 
@@ -21,7 +21,7 @@
  * SOFTWARE.
  */
 
-var version = "0.16.29";
+var version = "0.16.30";
 
 var extendStatics = function(d, b) {
     extendStatics = Object.setPrototypeOf ||
@@ -285,6 +285,8 @@ var NativeApis;
     NativeApis["SET_DYNAMIC_INDICATOR_STYLE"] = "setDynamicIndicatorStyle";
     NativeApis["EXTEND_DYNAMIC_INDICATOR"] = "extendDynamicIndicator";
     NativeApis["PROMPT_UPGRADE_REQUEST"] = "promptUpgradeRequest";
+    NativeApis["START_RTMS"] = "startRTMS";
+    NativeApis["STOP_RTMS"] = "stopRTMS";
     NativeApis["REMOVE_PARTICIPANT"] = "removeParticipant";
     NativeApis["SEND_KEYPAD_CONTROLS"] = "sendKeypadControls";
     NativeApis["GET_ZOOM_ROOM_DEVICE_DETAILS"] = "getZoomRoomDeviceDetails";
@@ -573,7 +575,7 @@ function wrapInMessageObject(value) {
     return value;
 }
 
-var _a$2, _b$1, _c$1, _d$1, _e$1, _f, _g, _h, _j, _k, _l, _m, _o, _p, _q, _r, _s, _t, _u, _v, _w, _x, _y, _z, _0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21, _22, _23, _24, _25, _26, _27, _28, _29, _30, _31, _32, _33, _34, _35, _36, _37, _38, _39, _40, _41, _42, _43, _44, _45, _46, _47, _48, _49, _50, _51, _52, _53, _54, _55, _56, _57, _58, _59, _60, _61, _62, _63, _64, _65, _66, _67, _68, _69, _70, _71, _72, _73, _74, _75, _76, _77, _78, _79, _80, _81, _82, _83, _84, _85, _86, _87, _88, _89, _90, _91, _92, _93, _94, _95, _96, _97, _98, _99, _100, _101, _102, _103, _104, _105, _106, _107, _108, _109, _110, _111, _112, _113, _114, _115, _116, _117, _118, _119, _120, _121, _122, _123, _124, _125, _126, _127, _128, _129, _130, _131, _132, _133, _134, _135, _136, _137, _138, _139, _140, _141, _142, _143, _144, _145;
+var _a$2, _b$1, _c$1, _d$1, _e$1, _f, _g, _h, _j, _k, _l, _m, _o, _p, _q, _r, _s, _t, _u, _v, _w, _x, _y, _z, _0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21, _22, _23, _24, _25, _26, _27, _28, _29, _30, _31, _32, _33, _34, _35, _36, _37, _38, _39, _40, _41, _42, _43, _44, _45, _46, _47, _48, _49, _50, _51, _52, _53, _54, _55, _56, _57, _58, _59, _60, _61, _62, _63, _64, _65, _66, _67, _68, _69, _70, _71, _72, _73, _74, _75, _76, _77, _78, _79, _80, _81, _82, _83, _84, _85, _86, _87, _88, _89, _90, _91, _92, _93, _94, _95, _96, _97, _98, _99, _100, _101, _102, _103, _104, _105, _106, _107, _108, _109, _110, _111, _112, _113, _114, _115, _116, _117, _118, _119, _120, _121, _122, _123, _124, _125, _126, _127, _128, _129, _130, _131, _132, _133, _134, _135, _136, _137, _138, _139, _140, _141, _142, _143, _144, _145, _146, _147, _148, _149;
 var compatibilityFnsApis = (_a$2 = {},
     _a$2[NativeApis.GET_APP_VARIABLE_LIST] = (_b$1 = {},
         _b$1[ZERO_SIXTEEN] = (_c$1 = {},
@@ -1287,6 +1289,24 @@ var compatibilityFnsApis = (_a$2 = {},
             },
             _145),
         _144),
+    _a$2[NativeApis.START_RTMS] = (_146 = {},
+        _146[ZERO_SIXTEEN] = (_147 = {},
+            _147[BASE_VERSION] = {
+                mapOutput: function (value) {
+                    return wrapInObject({ key: 'message', value: value });
+                },
+            },
+            _147),
+        _146),
+    _a$2[NativeApis.STOP_RTMS] = (_148 = {},
+        _148[ZERO_SIXTEEN] = (_149 = {},
+            _149[BASE_VERSION] = {
+                mapOutput: function (value) {
+                    return wrapInObject({ key: 'message', value: value });
+                },
+            },
+            _149),
+        _148),
     _a$2);
 
 var _a$1, _b, _c, _d, _e;
@@ -2972,6 +2992,20 @@ var ZoomSdk =  (function () {
         return __awaiter(this, void 0, void 0, function () {
             return __generator(this, function (_a) {
                 return [2 , this.callZoomApi(NativeApis.SEND_KEYPAD_CONTROLS, options)];
+            });
+        });
+    };
+    ZoomSdk.prototype.startRTMS = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                return [2 , this.callZoomApi(NativeApis.START_RTMS)];
+            });
+        });
+    };
+    ZoomSdk.prototype.stopRTMS = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                return [2 , this.callZoomApi(NativeApis.STOP_RTMS)];
             });
         });
     };
