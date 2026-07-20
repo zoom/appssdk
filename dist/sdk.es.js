bac5a3f284faa0673e657abd9423ba02e0f8605e
Kevin Zhang <43306846+KevZ1209@users.noreply.github.com>
2026-07-19 17:25:40 -0700

    0.16.40

--- dist/sdk.es.js
+++ dist/sdk.es.js
@@ -1,4 +1,4 @@
-/* Zoom Apps SDK v0.16.39  */
+/* Zoom Apps SDK v0.16.40  */
 /**
  * Copyright (c) 2026 Zoom Video Communications, Inc.
  * 
@@ -21,7 +21,7 @@
  * SOFTWARE.
  */
 
-var version = "0.16.39";
+var version = "0.16.40";
 
 var extendStatics = function(d, b) {
     extendStatics = Object.setPrototypeOf ||
